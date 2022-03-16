239ffbd6ef4d3b7c730cbd5d00c9d1badd7ce756
Lyndsey <lyndsey_davis2@baylor.edu>
2022-03-16 18:38:55 -0500

    finish csv

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# matplotlib_csv
-Advanced Python - Matplotlib - using CSV files
 
-this project is to teach students how to use CSV files in python
-this project also teaches students how to visualize the data using matplotlib
+The purpose of the CSV project was learn and demonstrate knowledge of matplotlib. 
+We progressed from simple plots to multiple subplots with more than one row of data.
+
+The final part of the project was the assignment which brought of the skills together with a little added complexity.
+We had to demonstrate our ability to pull data from two different sources to create a subplot on each source while not
+directly referencing the index number that corresponded to the column of data needed. We instead had to use the header
+row to call the index needed so that our code to form the two plots of the subplot were the same (except for the nuances
+that allowed the data to be called and stored for the two different sources).
+
+
+
 
---
+++ assignment.py
@@ -0,0 +1,75 @@
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+open_sitka = open("sitka_weather_2018_simple.csv", "r")
+open_dv = open("death_valley_2018_simple.csv", "r")
+
+sitka = csv.reader(open_sitka, delimiter=",")
+dv = csv.reader(open_dv, delimiter=",")
+
+header_row1 = next(sitka)
+header_row2 = next(dv)
+# print(type(header_row))
+
+# create index variables
+tmin1 = header_row1.index("TMIN")
+tmax1 = header_row1.index("TMAX")
+tmin2 = header_row2.index("TMIN")
+tmax2 = header_row2.index("TMAX")
+title1 = header_row1.index("NAME")
+title2 = header_row2.index("NAME")
+
+# create lists for highs, lows, and dates
+highs1, highs2, dates1, dates2, lows1, lows2 = [], [], [], [], [], []
+
+# add high and low temps for Sitka
+for temp in sitka:
+    try:
+        date = datetime.strptime(temp[2], "%Y-%m-%d")
+        high = int(temp[tmax1])
+        low = int(temp[tmin1])
+        sitka_title = temp[title1]
+    except ValueError:
+        print(f"Missing data for {date}")
+    else:
+        highs1.append(high)
+        dates1.append(date)
+        lows1.append(low)
+
+# add high and low temps for Death Valley
+for temp in dv:
+    try:
+        date = datetime.strptime(temp[2], "%Y-%m-%d")
+        high = int(temp[tmax2])
+        low = int(temp[tmin2])
+        dv_title = temp[title2]
+    except ValueError:
+        print(f"Missing data for {date}")
+    else:
+        highs2.append(high)
+        dates2.append(date)
+        lows2.append(low)
+
+# create plot
+fig = plt.figure()
+
+# subplots
+plt.subplot(2, 1, 1)
+plt.plot(dates1, highs1, color="red")
+plt.plot(dates1, lows1, color="blue")
+plt.fill_between(dates1, highs1, lows1, facecolor="blue", alpha=0.1)
+plt.title(sitka_title)
+
+plt.subplot(2, 1, 2)
+plt.plot(dates2, highs2, color="red")
+plt.plot(dates2, lows2, color="blue")
+plt.fill_between(dates2, highs2, lows2, facecolor="blue", alpha=0.1)
+plt.title(dv_title)
+
+# format date to be at an angle
+fig.autofmt_xdate()
+
+plt.suptitle("Daily High and Low Temps for Sitka and Death Valley 2018", fontsize=18)
+
+plt.show()
